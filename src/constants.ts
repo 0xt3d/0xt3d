f15a8d8ce8380168a680d8a58626a1473b5403d1
0xt3d <Aditya.Patil0404@gmail.com>
2025-08-03 20:35:06 +0530

    Updated Blog and README.md
    Cleaned up template and added HelloWorld Post and Updated Readme File

--- src/constants.ts
+++ src/constants.ts
@@ -19,25 +19,25 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/0xt3d",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/404",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/teadi/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:t3d@0xt3d.tech",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
---
+++ src/data/blog/helloworld.md
@@ -0,0 +1,14 @@
+---
+author: Aditya
+pubDatetime: 2025-08-23T15:22:00Z
+modDatetime: 2025-06-13T16:52:45.934Z
+title: Hello World
+slug: hello-world
+featured: true
+draft: false
+tags:
+  - blog
+description: Getting Started with Astro Blog
+---
+
+Hello World!
