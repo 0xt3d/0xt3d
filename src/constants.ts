086cf1ce2b1fa97a8b30fe6d812609d47fa4a59e
0xt3d <Aditya.Patil0404@gmail.com>
2025-09-07 17:37:43 +0530

    Cleaning-up

--- src/constants.ts
+++ src/constants.ts
@@ -25,7 +25,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "X",
-    href: "https://x.com/404",
+    href: "https://x.com/0xt3d",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
